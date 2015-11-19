4ca13e70012a52c5a7037681f2dbd874feeaed98
crazyconv <conanconv@163.com>
2015-11-19 14:23:53 +0800

    Trie and SuffixTree

--- template/SearchPattern.java
+++ template/SearchPattern.java
@@ -23,6 +23,7 @@ public class SearchPattern{
     /**********************************************************************
      **                                KMP                               **
      **********************************************************************/
+    // http://www.geeksforgeeks.org/searching-for-patterns-set-2-kmp-algorithm/
     // O(n)
     public int[] computeLPSArray(String s){
         int[] lps = new int[s.length()];
@@ -66,6 +67,7 @@ public class SearchPattern{
     /**********************************************************************
      **                           Rabin-Karp                             **
      **********************************************************************/
+    // http://www.geeksforgeeks.org/searching-for-patterns-set-3-rabin-karp-algorithm/
     // average/best: O(m+n); worst: O(mn)
     public void RabinKarpSearch(String s, String p){
         int prime = 101, d = 256;
@@ -98,10 +100,86 @@ public class SearchPattern{
         }
     }
 
+    /**********************************************************************
+     **                         Finite Automata                          **
+     **********************************************************************/
+    // http://www.geeksforgeeks.org/pattern-searching-set-5-efficient-constructtion-of-finite-automata/
+    // O(m+n)
+    public int[][] computeTransTable(String s){
+        int[][] t = new int[s.length()+1][256];
+        for(int i = 0; i < 256; i++){
+            t[0][i] = 0;
+        }
+        t[0][s.charAt(0)] = 1;
+        int lps = 0;
+        for(int i = 1; i <= s.length(); i++){
+            System.arraycopy(t[lps], 0, t[i], 0, 256);
+            if(i < s.length()){
+                t[i][s.charAt(i)] = i + 1;
+                lps = t[lps][s.charAt(i)];
+            }
+        }
+        return t;
+    }
+    public void FASearch(String s, String p){
+        int[][] t = computeTransTable(p);
+        int state = 0;
+        for(int i = 0; i < s.length(); i++){
+            state = t[state][s.charAt(i)];
+            if(state == p.length())
+                System.out.println("Found pattern at index " + String.valueOf(i - p.length() + 1));
+        }
+    }
+
+    /**********************************************************************
+     **                            Boyer Moore                           **
+     **                      Bad Character Heuristic                     **
+     **********************************************************************/
+    // http://www.geeksforgeeks.org/pattern-searching-set-7-boyer-moore-algorithm-bad-character-heuristic/
+    // O(m+n)
+    public int[] computeBadCharTable(String p){
+        int[] t = new int[256];
+        for(int i = 0; i < p.length(); i++){
+            t[p.charAt(i)] = i;
+        }
+        return t;
+    }
+
+    public void badCharacterHeuristic(String s, String p){
+        int[] t = computeBadCharTable(p);
+        int i = 0, j = 0;
+        int sLen = s.length(), pLen = p.length();
+        while(i <= sLen - pLen){
+            for(j = pLen - 1; j >= 0; j--){
+                if(s.charAt(i+j) != p.charAt(j))
+                    break;
+            }
+            if(j < 0){
+                System.out.println("Found pattern at index " + String.valueOf(i));
+                if(i == sLen - pLen)
+                    break;
+                else
+                    i += pLen - t[s.charAt(i+pLen)];
+            } else {
+                if(j > t[s.charAt(i+j)])
+                    i += j - t[s.charAt(i+j)];
+                else
+                    i++;
+            }
+        }
+    }
+
+    /**********************************************************************
+     **                            Suffix Tree                           **
+     **********************************************************************/
+    // see SuffixTree.java
+
     public static void main(String[] args){
         SearchPattern sp = new SearchPattern();
         // sp.KMPSearch("hellyhelloxy", "hello");
         // sp.RabinKarpSearch("hellyhelloxyhello", "hello");
-        sp.naiveSearch("hellyhelloxyhello", "hello");
+        // sp.naiveSearch("hellyhelloxyhello", "hello");
+        // sp.FASearch("hellyhelloxyhello", "hello");
+        sp.badCharacterHeuristic("hellyhelloxyhello", "hello");
     }
 }
---
+++ template/Trie.java
@@ -0,0 +1,95 @@
+public class Trie{
+    int count;
+    Node root;
+    public Trie(){
+        count = 0;
+        root = new Node();
+    }
+
+    // O(n)
+    public void insert(String s){
+        count ++;
+        Node node = root;
+        for(int i = 0; i < s.length(); i++){
+            if(node.children[s.charAt(i)] == null){
+                node.children[s.charAt(i)] = new Node();
+            }
+            node = node.children[s.charAt(i)];
+        }
+        node.value = count;
+    }
+
+    // O(n)
+    public boolean search(String s){
+        Node node = root;
+        for(int i = 0; i < s.length(); i++){
+            if(node.children[s.charAt(i)] == null){
+                return false;
+            }
+            node = node.children[s.charAt(i)];
+        }
+        return node.value != 0;
+    }
+
+    // O(n)
+    public void delete(String s){
+        deleteHelper(root, s, -1);
+    }
+    // result indicates whether the node can be delete
+    public boolean deleteHelper(Node node, String s, int cur){
+        if(node != null){
+            if(cur == s.length() - 1){ // end of s
+                if(isLeaf(node) && !hasChildren(node))
+                    return true;
+                else
+                    node.value = 0;
+            } else {
+                if(deleteHelper(node.children[s.charAt(cur+1)], s, cur+1)){
+                    node.children[s.charAt(cur+1)] = null;
+                    if(!isLeaf(node) && !hasChildren(node)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    public boolean isLeaf(Node node){
+        return node.value != 0;
+    }
+    public boolean hasChildren(Node node){
+        for(int i = 0; i < 256; i ++){
+            if(node.children[i] != null)
+                return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] argvs){
+        Trie trie = new Trie();
+        String[] s = {"she", "sells", "sea", "shore", "the", "by", "sheer"};
+        for(int i = 0; i < s.length; i ++){
+            trie.insert(s[i]);
+        }
+        trie.delete("the");
+        trie.delete("sea");
+        trie.delete("she");
+        for(int i = 0; i < s.length; i++){
+            String result = null;
+            if(trie.search(s[i])){
+                result = " in ";
+            } else {
+                result = " not in ";
+            }
+            System.out.println(s[i] + result + "trie");
+        }
+    }
+}
+class Node{
+    int value;
+    Node[] children;
+    public Node(){
+        value = 0;
+        children = new Node[256];
+    }
+}
